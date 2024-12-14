ed014eaf05d4baf9f2328656c9ec4b6a785a16d2
R-Artyom <ryabov.artyom@gmail.com>
2024-12-14 21:40:44 +0700

    v2 Изменён алгоритм считывания вакансий: Теперь запуск считывания происходит раз в минуту, после запуска скрипт в течение 57 сек считывает вакансии с паузами от 10 мс до 100 мс. До этого считывание происходило раз в секунду (т.е. не более одной вакансии в сек)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // Запуск парсинга вакансии
-        $schedule->command('parse-vacancy:run')->everySecond();
+        $schedule->command('parse-vacancy:run')->everyMinute();
     }
 
     /**
--- app/Http/Controllers/Vacancies/RunParseController.php
+++ app/Http/Controllers/Vacancies/RunParseController.php
@@ -30,81 +30,96 @@ class RunParseController extends Controller
             $counter->status = 'busy';
             $counter->update(['value' => $vacancyId]);
 
-            // Блок для выброса исключений
-            try {
-                // Создание транзакции
-                DB::beginTransaction();
+            // Начать отсчет времени
+            $startTime = microtime(true);
+            // Промежуточная отметка времени
+            $fixedTime = microtime(true);
 
-                if ($vacancyId < $counter->limit) {
-                    // Если ещё нет такой вакансии в базе MySql
-                    if (!Vacancy::query()->where('id', $vacancyId)->exists()) {
-                        // Запрос данных о вакансии
-                        $response = Http::get("https://api.hh.ru/vacancies/$vacancyId");
-                        if ($response->successful()) {
-                            $vacancyData = $response->json();
-                            // Если есть ссылка на работодателя
-                            if (isset($vacancyData['employer']['id'])) {
-                                $employerId = $vacancyData['employer']['id'];
-                                // Если работодатель указан, то сначала надо записать данные о нём
-                                if (!empty($employerId)) {
-                                    // Если не существует такого работодателя в базе MySql
-                                    if (!Employer::query()->where('id', $employerId)->exists()) {
-                                        $response = Http::get("https://api.hh.ru/employers/{$employerId}");
-                                        $data = $response->json();
-                                        if ($response->successful()) {
-                                            (new EmployersStoreController)($data);
-                                        } else {
-                                            // Если в базе hh нет такого работодателя, то пустая запись
-                                            (new EmployersStoreController)(['id' => $employerId]);
+            // Повторять считывание вакансий в течение 57 сек
+            while ($fixedTime - $startTime < 57) {
+                // Задержка от 10 мс до 100 мс
+                usleep(rand(10000, 100000));
+
+                // Блок для выброса исключений
+                try {
+                    // Создание транзакции
+                    DB::beginTransaction();
+
+                    if ($vacancyId < $counter->limit) {
+                        // Если ещё нет такой вакансии в базе MySql
+                        if (!Vacancy::query()->where('id', $vacancyId)->exists()) {
+                            // Запрос данных о вакансии
+                            $response = Http::get("https://api.hh.ru/vacancies/$vacancyId");
+                            if ($response->successful()) {
+                                $vacancyData = $response->json();
+                                // Если есть ссылка на работодателя
+                                if (isset($vacancyData['employer']['id'])) {
+                                    $employerId = $vacancyData['employer']['id'];
+                                    // Если работодатель указан, то сначала надо записать данные о нём
+                                    if (!empty($employerId)) {
+                                        // Если не существует такого работодателя в базе MySql
+                                        if (!Employer::query()->where('id', $employerId)->exists()) {
+                                            $response = Http::get("https://api.hh.ru/employers/{$employerId}");
+                                            $data = $response->json();
+                                            if ($response->successful()) {
+                                                (new EmployersStoreController)($data);
+                                            } else {
+                                                // Если в базе hh нет такого работодателя, то пустая запись
+                                                (new EmployersStoreController)(['id' => $employerId]);
+                                            }
                                         }
                                     }
+                                } else {
+                                    $vacancyData['employer']['id'] = null;
                                 }
-                            } else {
-                                $vacancyData['employer']['id'] = null;
-                            }
 
-                            // Запись данных о вакансии
-                            (new StoreController)($vacancyData);
+                                // Запись данных о вакансии
+                                (new StoreController)($vacancyData);
+                            }
                         }
+                        // Инкремент счетчика с сохранением в базе
+                        $vacancyId++;
+                        $counter->update(['value' => $vacancyId]);
                     }
-                    // Инкремент счетчика с сохранением в базе
-                    $vacancyId++;
-                    $counter->update(['value' => $vacancyId]);
+
+                    // Фиксирование транзакции
+                    DB::commit();
+
+                // Блок перехвата исключений
+                } catch (ConnectionException $e) {
+                    // Откат транзакции
+                    DB::rollBack();
+                    logger()->error('Ошибка соединения ' . '(' . route('vacancies.run') . ')',
+                        [
+                            'vacancyId' => $vacancyId,
+                            'message' => $e->getMessage()
+                        ]
+                    );
+                } catch (\Exception $e) {
+                    // Откат транзакции
+                    DB::rollBack();
+                    $counter->update(['status' => 'error']);
+                    // Логирование в файл
+                    logger()->error('Ошибка общая ' . '(' . route('vacancies.run') . ')',
+                        [
+                            'vacancyId' => $vacancyId,
+                            'error' => $e->getMessage(),
+                        ]
+                    );
+                    return;
                 }
 
-                // Счетчик свободен
-                $counter->status = 'run';
-                $counter->update(['value' => $vacancyId]);
-
-                // Фиксирование транзакции
-                DB::commit();
-
-            // Блок перехвата исключений
-            } catch (ConnectionException $e) {
-                // Откат транзакции
-                DB::rollBack();
-                // Счетчик свободен
-                $counter->status = 'run';
-                $counter->update(['value' => $vacancyId]);
-                logger()->error('Ошибка соединения ' . '(' . route('vacancies.run') . ')',
-                    [
-                        'vacancyId' => $vacancyId,
-                        'message' => $e->getMessage()
-                    ]
-                );
-            // Общая ошибка
-            } catch (\Exception $e) {
-                // Откат транзакции
-                DB::rollBack();
-                $counter->update(['status' => 'error']);
-                // Логирование в файл
-                logger()->error('Ошибка общая ' . '(' . route('vacancies.run') . ')',
-                    [
-                        'vacancyId' => $vacancyId,
-                        'error' => $e->getMessage(),
-                    ]
-                );
+                // Фиксировать отметку времени
+                $fixedTime = microtime(true);
             }
+            // Если скрипт выполнялся дольше минуты
+            if ($fixedTime - $startTime > 60) {
+                // Логирование в файл
+                logger()->error('Время выполнения скрипта > 60 сек ' . '(' . route('vacancies.run') . ')');
+            }
+            // Счетчик свободен
+            $counter->status = 'run';
+            $counter->update(['value' => $vacancyId]);
         }
     }
 }
