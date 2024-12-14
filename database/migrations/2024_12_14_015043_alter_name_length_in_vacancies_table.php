884a8cde174166c6ea8fe2ef9f0280c4e2fa232e
R-Artyom <ryabov.artyom@gmail.com>
2024-12-14 19:18:20 +0700

    Увеличена длина поля name в таблице "Вакансии" до 255 символов

---
+++ database/migrations/2024_12_14_015043_alter_name_length_in_vacancies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vacancies', function (Blueprint $table) {
+            // Увеличение длины до 255 символов
+            $table->string('name', 255)->nullable()->comment('Название')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vacancies', function (Blueprint $table) {
+            // Возврат старого значения длины
+            $table->string('name', 200)->nullable()->comment('Название')->change();
+        });
+    }
+};
