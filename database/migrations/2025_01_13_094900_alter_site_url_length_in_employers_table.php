f5556bc275ddf800a06fb9236b574c6d44e9fcba
R-Artyom <ryabov.artyom@gmail.com>
2025-01-13 12:56:54 +0700

    Увеличена длина поля site_url в таблице "Работодатель" до 255 символов

---
+++ database/migrations/2025_01_13_094900_alter_site_url_length_in_employers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employers', function (Blueprint $table) {
+            // Увеличение длины до 255 символов
+            $table->string('site_url', 255)->nullable()->comment('Сайт')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employers', function (Blueprint $table) {
+            // Возврат старого значения длины
+            $table->string('site_url', 200)->nullable()->comment('Сайт')->change();
+        });
+    }
+};
