fafa57791418c49e3158e86a1ae02dc312c7bb6d
R-Artyom <ryabov.artyom@gmail.com>
2025-04-27 14:37:07 +0700

    Миграция для изменения типа атрибута status на ENUM с значениями run, error, busy

---
+++ database/migrations/2025_04_27_064339_change_status_type_in_counters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('counters', function (Blueprint $table) {
+            $table->enum('status', ['run', 'error', 'busy'])
+                ->default('run')
+                ->comment('Статус')
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('counters', function (Blueprint $table) {
+            $table->string('status', 100)
+                ->default('run')
+                ->comment('Статус')
+                ->change();
+        });
+    }
+};
