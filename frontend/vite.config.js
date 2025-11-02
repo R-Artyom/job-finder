ee9dc8e608d22a9b30ae755ac136eb76e9e57c6a
R-Artyom <ryabov.artyom@gmail.com>
2025-11-02 19:03:53 +0700

    Установка Tailwind CSS как плагина Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,14 @@
 import { defineConfig } from 'vite'
 // Плагин для сборки Vue-компонентов
 import vue from '@vitejs/plugin-vue'
+// СSS-фреймворк
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // Регистрация плагинов в Vite
   plugins: [
     vue(),
+    tailwindcss(),
   ],
   // Настройка дев-сервера при запуске "npm run dev", перехват и проксирование запросов /api на http://172.17.0.1:8080/api/
   server: {
