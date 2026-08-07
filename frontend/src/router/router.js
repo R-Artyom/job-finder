713764802a7166b5b1be17bc26c1754050a365b2
R-Artyom <ryabov.artyom@gmail.com>
2026-08-07 15:37:31 +0700

    front: Абсолютно все неизвестные маршруты временно перенаправляются на страницу вакансий. Сборка файлов для прода

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -4,9 +4,18 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
+            path: '/',
+            redirect: '/vacancies'
+        },
+        {
             path: '/vacancies',
             name: 'vacancies.index',
             component: () => import('../pages/vacancies/index.vue')
+        },
+        // Все неизвестные маршруты перенаправляются на страницу вакансий
+        {
+            path: '/:pathMatch(.*)*',
+            redirect: '/vacancies'
         }
     ],
 })
